9003d8591697bf0152e06e591100bafdeb25d818
Suon Samoun <samoun.suon@student.passerellesnumeriques.org>
2025-07-18 18:53:27 +0700

    update home page

---
+++ wp-content/plugins/ultimate-blocks/dist/blocks.build.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-keycodes', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => '53a3a8220a9a122707dd');
