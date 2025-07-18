3089b6b4851573b9b00dd5b3161ee0782c369678
Suon Samoun <samoun.suon@student.passerellesnumeriques.org>
2025-07-18 19:18:21 +0700

    our program

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define( 'WP_CACHE', true );
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'bondoskomar' );
+define( 'DB_NAME', 'bondoskomar_db' );
 
 
 /** Database username */
